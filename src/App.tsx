808ba34b821af1e1b1d62d8a61b0807391067e7d
pro1001000r <pro1001000r@ya.ru>
2025-08-09 16:15:17 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Col, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Col>Новый модуль</Col>
+    </Container>
+  );
+};
+export default App;
