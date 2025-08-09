2c187f1a9b4f26d973cdc7867f2d99c0673c7e8c
pro1001000r <pro1001000r@ya.ru>
2025-08-10 01:41:16 +0300

    1 написание в инпуте и передача в центр

--- src/components/ScreenTodo.tsx
+++ src/components/ScreenTodo.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./NavBar";
+import TodoForm from "./TodoForm";
 
-const ScreenTodo:React.FC = () => {
+const ScreenTodo: React.FC = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addHandler = (title:string) => {
+       
+     
+     //консоль 10 Август 2025 (воскресенье)
+     console.log('>>>> title из (ScreenTodo):', title); //консоль
+     
+  };
   return (
     <>
       <NavBar />
-      <div></div>
+      <TodoForm onAdd={addHandler}/>
     </>
   );
 };
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
- 
-const TodoForm: React.FC = () => {
- 
-    return (
-    <>
-     Новый модуль
-    </>
-     );
+import React, { useRef, useState } from "react";
+import { Container, Form, InputGroup } from "react-bootstrap";
+
+interface TodoFormProps {
+  onAdd(title: string): void;
 }
-export default TodoForm
+
+//const TodoForm: React.FC<{onAdd(title:string):void}> = (props) => {
+
+const TodoForm: React.FC<TodoFormProps> = (props) => {
+  const [title, setTitle] = useState<string>("");
+  const ref = useRef<HTMLInputElement>(null); //2 способ
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      //консоль 10 Август 2025 (воскресенье)
+      
+      console.log(">>>> keyPressHandler из (TodoForm):", title); //консоль
+      console.log(">>>> ref из (TodoForm):", ref.current!.value); //2 способ
+
+      //передаем во внешку!!!!!
+      props.onAdd(title);
+
+      //очищаем
+      ref.current!.value = ""; //2 способ
+      setTitle("");
+    }
+  };
+
+  return (
+    <>
+      <Container>
+        <InputGroup>
+          {/* <input type="text" name="todo" id="title" /> */}
+          <Form.Control
+            ref={ref} //2 способ
+            value={title}
+            type="text"
+            onChange={changeHandler}
+            onKeyPress={keyPressHandler}
+            placeholder="Введите название дела"
+          />
+        </InputGroup>
+      </Container>
+    </>
+  );
+};
+export default TodoForm;
