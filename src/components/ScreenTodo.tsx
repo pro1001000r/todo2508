0c9b36ba0c1a25231061a4e0fe1d1fbd5c104765
pro1001000r <pro1001000r@ya.ru>
2025-08-11 00:05:06 +0300

    todo

--- src/components/ScreenTodo.tsx
+++ src/components/ScreenTodo.tsx
@@ -1,21 +1,77 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import { ITodo } from "./inrefaces";
+
+declare var confirm: (q: string) => boolean; //объявление типа confirm
 
 const ScreenTodo: React.FC = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
-  const addHandler = (title:string) => {
-       
-     
-     //консоль 10 Август 2025 (воскресенье)
-     console.log('>>>> title из (ScreenTodo):', title); //консоль
-     
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem("todos") || "[]") as ITodo[]; //ЛайфХак с пустым значением
+    
+    //консоль 10 Август 2025 (воскресенье)
+    console.log('>>>> todos из (ScreenTodo):', saved); //консоль
+    
+    setTodos(saved);
+  }, []);
+
+  
+
+  const addHandler = (title: string) => {
+    //консоль 10 Август 2025 (воскресенье)
+    console.log(">>>> title из (ScreenTodo):", title); //консоль
+
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false,
+    };
+
+    //setTodos([newTodo, ...todos])
+    setTodos((prev) => [newTodo, ...prev]);
   };
+
+  const toggleHandler = (id: number) => {
+    
+    //консоль 11 Август 2025 (понедельник)
+    console.log('>>>> checkbox id:', id); //консоль
+    
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+          console.log('>>>> todo.completed id:', todo.completed); //консоль
+          console.log('>>>> checkbox id:', id); //консоль
+    
+        }
+        return todo;
+      })
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    const isrem = confirm("вы уверены что хотите удалить?"); //!!!!!!! confirm
+    if (isrem) {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
   return (
     <>
       <NavBar />
-      <TodoForm onAdd={addHandler}/>
+      <TodoForm onAdd={addHandler} />
+      <TodoList
+        todos={todos}
+        onToggle={toggleHandler}
+        onRemove={removeHandler}
+      />
     </>
   );
 };
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  FormGroup,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import { ITodo } from "./inrefaces";
+
+type TodoListProps = {
+  todos: ITodo[];
+  onToggle(id: number): void; //1 описание как функция
+  onRemove: (id: number) => void; //2 описание как стрелочная
+};
+
+const TodoList: React.FC<TodoListProps> = ({ todos, onToggle, onRemove }) => {
+  if (todos.length === 0) {
+    return <p>Пока дел нет</p>;
+  }
+
+  return (
+    <>
+      {todos.map((todo) => {
+        return (
+          <p key={todo.id}>
+            <Form.Check // prettier-ignore
+              //type={"switch"} // или "сheckbox"
+              type = {"checkbox"} // или "сheckbox"
+              //id={String(todo.id)}
+              //   label={`разрешен вход в систему`}
+              //value={todo.completed}
+              //checked={todo.completed}
+              onChange={onToggle.bind(null, todo.id)}
+            />
+            {todo.title}
+            <div className="btn" onClick={() => onRemove(todo.id)}>
+              Delete
+            </div>
+          </p>
+        );
+      })}
+    </>
+  );
+};
+export default TodoList;
